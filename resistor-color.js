8928647c077764227fcd919d84438a13a86cbe6d
Pablo <Pablo@Pablos-MacBook-Pro.local>
2019-11-04 21:15:42 -0800

    Practice JavaScript

--- resistor-color.js
+++ resistor-color.js
@@ -1,14 +1,9 @@
 
-export const colorCode = (inputColor) => {
-  if (!inputColor) {
-    return colors;
-  }
-  const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-  for (const color of colors) {
-    if (inputColor === color) {
+const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
+export const colorCode = (color) => {
       return colors.indexOf(color);
-    }
-  }
+  
 };
 
 export const COLORS = undefined;
